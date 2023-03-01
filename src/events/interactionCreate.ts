b4dcb54599b6988a2bc2ff88b435b22ab1a95d39
xMizuki <62658861+xMizuki@users.noreply.github.com>
2023-03-01 23:17:12 +0100

    Updated events

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,4 +1,4 @@
-import type { EventFile } from "../@types";
+import type { EventFile, RPGUserDataJSON, RPGUserQuest, UseXCommandQuest } from "../@types";
 import { Events, Interaction } from "discord.js";
 import JolyneClient from "../structures/JolyneClient";
 import CommandInteractionContext from "../structures/CommandInteractionContext";
@@ -58,26 +58,71 @@ const Event: EventFile = {
                 );
                 ctx = new CommandInteractionContext(interaction, userData);
                 if (!ctx.userData) {
-                    if (
-                        command.data.name === "adventure" &&
-                        interaction.options.getSubcommand() !== "start"
-                    ) {
-                        return interaction.reply({
-                            content: interaction.client.translations.get("en-US")(
-                                "base:NO_ADVENTURE",
-                                {
-                                    emojis: interaction.client.localEmojis,
-                                }
-                            ),
-                        });
-                    }
-                    return interaction.reply({
-                        content: interaction.client.translations.get("en-US")("base:NO_ADVENTURE", {
-                            emojis: interaction.client.localEmojis,
-                        }),
-                    });
+                    if (command.data.name === "adventure") {
+                        if (interaction.options.getSubcommand() !== "start")
+                            return ctx.sendTranslated("base:NO_ADVENTURE");
+                    } else return ctx.sendTranslated("base:NO_ADVENTURE");
                 }
             } else ctx = new CommandInteractionContext(interaction);
+
+            if (command.category === "rpg" && ctx.userData) {
+                let commandName = command.data.name;
+                if (command.data.options.filter((r) => r.type === 1).length !== 0) {
+                    commandName += ` ${interaction.options.getSubcommand()}`;
+                }
+                const oldDataJSON = JSON.stringify(ctx.userData);
+
+                for (const quests of [
+                    ctx.userData.daily.quests,
+                    ctx.userData.chapter.quests,
+                    ...ctx.userData.sideQuests.map((v) => v.quests),
+                ]) {
+                    for (const quest of quests) {
+                        if (Functions.isUseXCommandQuest(quest) && quest.command === commandName) {
+                            quest.amount++;
+                        }
+
+                        if (Functions.isWaitQuest(quest) && !quest.claimed) {
+                            if (quest.end < Date.now()) {
+                                quest.claimed = true;
+                                if (quest.email) {
+                                    Functions.addEmail(ctx.userData, quest.email);
+                                }
+                                if (quest.quest) {
+                                    const questData = Functions.findQuest(quest.quest);
+                                    // find a way to find if from daily, quests or sidequests
+                                    if (questData) {
+                                        if (
+                                            ctx.userData.chapter.quests.find(
+                                                (r) => r.id === quest.id
+                                            )
+                                        )
+                                            ctx.userData.chapter.quests.push(
+                                                Functions.pushQuest(questData)
+                                            );
+                                        else if (
+                                            ctx.userData.daily.quests.find((r) => r.id === quest.id)
+                                        )
+                                            ctx.userData.daily.quests.push(
+                                                Functions.pushQuest(questData)
+                                            );
+                                        else {
+                                            for (const sideQuest of ctx.userData.sideQuests) {
+                                                if (sideQuest.quests.find((r) => r.id === quest.id))
+                                                    sideQuest.quests.push(
+                                                        Functions.pushQuest(questData)
+                                                    );
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                if (oldDataJSON !== JSON.stringify(ctx.userData))
+                    ctx.client.database.saveUserData(ctx.userData);
+            }
 
             try {
                 await command.execute(ctx);
@@ -92,6 +137,21 @@ const Event: EventFile = {
                     }) // eslint-disable-next-line @typescript-eslint/no-empty-function
                     .catch(() => {});
             }
+        } else if (interaction.isAutocomplete()) {
+            const command = interaction.client.commands.get(interaction.commandName);
+            if (!command || !interaction.guild) return;
+
+            const userData = await interaction.client.database.getRPGUserData(interaction.user.id);
+            if (!userData) {
+                interaction.respond([]);
+                return;
+            }
+
+            command.autoComplete(
+                interaction,
+                userData,
+                interaction.options.getFocused().toString()
+            );
         }
     },
 };
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,8 +1,9 @@
-import type { EventFile } from "../@types";
+import type { EventFile, RPGUserDataJSON } from "../@types";
 import * as Functions from "../utils/Functions";
 import { Events, ActivityType, ActivityOptions } from "discord.js";
 import * as Stands from "../rpg/Stands/Stands";
 import Jolyne from "../structures/JolyneClient";
+import { CronJob } from "cron";
 
 const Event: EventFile = {
     name: Events.ClientReady,
@@ -12,9 +13,26 @@ const Event: EventFile = {
             type: ActivityType.Watching,
             name: "bugs...",
         });
+        fetchPatreonsFromCache();
+
+        async function resetDailyQuests() {
+            const RPGUsers = (await client.database.postgresql
+                .query(`SELECT * FROM "RPGUsers"`)
+                .then((r) => r.rows)) as RPGUserDataJSON[];
+
+            for (const RPGUser of RPGUsers) {
+                console.log(RPGUser.id, "reset daily");
+                RPGUser.daily.quests = Functions.generateDailyQuests(RPGUser.level);
+                client.database.saveUserData(RPGUser);
+
+                // removing claimed daily quests from cache
+                client.database.redis.del(`daily-quests-${RPGUser.id}`);
+            }
+        }
 
         // prettier-ignore
         if (parseInt(process.env.CLUSTER + 1) === parseInt(process.env.CLUSTER_COUNT)) {
+            
 			const lastCommands = await client.database.getString(
 				`jolyne_${client.user.id}:commands`
 			);
@@ -47,7 +65,16 @@ const Event: EventFile = {
 				);
 				client.log("Updated private slash commands");
 			}
+
+    
+            const dailyQuestsJob = new CronJob("0 0 * * *", resetDailyQuests, null, true, "Europe/Paris");
+            dailyQuestsJob.start();
+    
+    
+            client.log("Started daily quests cron job", "ready");
+    
 		}
+        client._cachedCommands = await client.application.commands.fetch();
 
         // end
         client.user.setActivity({
@@ -77,6 +104,63 @@ const Event: EventFile = {
         }, 1000 * 60 * 5);
 
         client.log(`Logged in as ${client.user?.tag}`, "ready");
+
+        const fetchPatreonsJob = new CronJob(
+            "0 0 * * *",
+            fetchPatreons,
+            null,
+            true,
+            "Europe/Paris"
+        );
+        fetchPatreonsJob.start();
+        fetchPatreons();
+
+        async function fetchPatreonsFromCache() {
+            const patrons = await client.database.redis.keys("patron:*");
+            for (const patron of patrons) {
+                const tier = await client.database.redis.get(patron);
+                client.patreons.push({
+                    id: patron.split(":")[1],
+                    level: parseInt(tier),
+                });
+            }
+        }
+
+        async function fetchPatreons() {
+            // fetching patrons, second priority to not make the bot laod slower
+            // prettier-ignore
+            if (parseInt(process.env.CLUSTER + 1) === parseInt(process.env.CLUSTER_COUNT)) {
+            const patrons = await client.fetchPatrons();
+    
+            client.log("Clearing old patrons", "ready");
+    
+            const keys = await client.database.redis.keys("patron:*");
+            for (const key of keys) client.database.redis.del(key);
+    
+            for (const patron of patrons) {
+                if (new Date(patron.last_charge_date).getTime() + 1000 * 60 * 60 * 24 * 31 < Date.now())
+                    {
+                        client.log(`Fetched former patron ${patron.full_name}`);
+                        continue;
+                    }
+                let tier;
+    
+                if (patron.currently_entitled_amount_cents >= 1600) tier = 4; // OVER HEAVEN SUPPORTER
+                else if (patron.currently_entitled_amount_cents >= 1000) tier = 3; // HEAVEN ASCENDED SUPPORTER
+                else if (patron.currently_entitled_amount_cents >= 450) tier = 2; // ASCENDED SUPPORTER
+                else tier = 1; // SUPPORTER
+    
+                client.log(`Fetched patron ${patron.full_name} is tier ${tier} with money ${patron.currently_entitled_amount_cents} (discordID: ${patron.discord_id})`);
+                client.database.redis.set(`patron:${patron.discord_id}`, String(tier));
+                client.patreons.push({
+                    id: patron.discord_id,
+                    level: tier,
+                });
+            }
+        } else {
+            setTimeout(fetchPatreonsFromCache, 1000 * 15);
+        }
+        }
     },
 };
 export default Event;
