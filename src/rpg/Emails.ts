9ebd3e5b1f0834fd0a184e36eba9c90f7519c11c
mizufare <62658861+mizufare@users.noreply.github.com>
2024-02-04 19:49:25 +0100

    🚀 Update rpg files.

--- src/rpg/Emails.ts
+++ src/rpg/Emails.ts
@@ -241,7 +241,7 @@ export const SecondAnniversary: Email = {
     author: NPCs.JolyneTeam,
     subject: "2nd Anniversary",
     content: (ctx) =>
-        `It's been 2 years since the bot was created.\nInitially, the bot was created to be a fun private bot for a small group of friends, but it has grown to be a bot with over 450k users.\nJolyne wouldn't be where it is today without the support of the community, and our patreons.\n\nWe would like to thank everyone who has supported us, and we hope to continue to provide a fun and enjoyable experience for everyone.`,
+        `It's been 2 years since the bot was created.\nInitially, the bot was created to be a fun private bot for a small group of friends, but it has grown to be a bot with over 15k registered players.\nJolyne wouldn't be where it is today without the support of the community, and our patreons.\n\nWe would like to thank everyone who has supported us, and we hope to continue to provide a fun and enjoyable experience for everyone.`,
     footer: "Thank you for being part of the Jolyne community!",
     emoji: "🎉",
     rewards: {
