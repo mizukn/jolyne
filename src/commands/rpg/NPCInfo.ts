ac2a65e3401c96959a11f93da66f2e5e7b94f846
xMizuki <62658861+xMizuki@users.noreply.github.com>
2024-08-17 13:16:41 +0200

    🌟 Update commands files.

--- src/commands/rpg/NPCInfo.ts
+++ src/commands/rpg/NPCInfo.ts
@@ -24,6 +24,7 @@ const slashCommand: SlashCommandFile = {
                 description: "The NPC you want to see the profile of",
                 type: 3,
                 required: true,
+                autocomplete: true,
             },
         ],
     },
