03b7223bc56091d2b8ff5a7040cff79ed0d6eb2d
xMizuki <62658861+xMizuki@users.noreply.github.com>
2024-11-02 14:42:50 +0100

    xp box nerf

--- src/rpg/Items/SpecialItems.ts
+++ src/rpg/Items/SpecialItems.ts
@@ -1022,8 +1022,8 @@ export const XPBox: Special = {
                     {
                         percent: 100,
                         xp: Functions.randomNumber(
-                            Functions.getMaxXp(ctx.userData.level ?? 1) * 0.2,
-                            Functions.getMaxXp(ctx.userData.level ?? 1) * 0.4
+                            Functions.getMaxXp(ctx.userData.level ?? 1) * 0.1,
+                            Functions.getMaxXp(ctx.userData.level ?? 1) * 0.3
                         ),
                     },
                 ],
