74f1d65ebaf1cbfc1e93c867be2292665484fe42
xMizuki <62658861+xMizuki@users.noreply.github.com>
2026-02-03 19:52:01 +0100

    🚀 Update rpg files.

--- src/rpg/Events/4thAnniversaryEvent.ts
+++ src/rpg/Events/4thAnniversaryEvent.ts
@@ -1,7 +1,7 @@
 import { SlashCommand } from "../../@types";
 import CommandInteractionContext from "../../structures/CommandInteractionContext";
 
-export const startOf4rdAnnivesaryEvent = new Date("2026-03-06");
-export const endOf4rdAnnivesaryEvent = new Date("2026-03-30");
-export const is4rdAnnivesaryEvent = (): boolean =>
-    new Date() >= startOf4rdAnnivesaryEvent && new Date() <= endOf4rdAnnivesaryEvent;
+export const startOf4thAnnivesaryEvent = new Date("2026-03-06");
+export const endOf4thAnnivesaryEvent = new Date("2026-03-30");
+export const is4thAnnivesaryEvent = (): boolean =>
+    new Date() >= startOf4thAnnivesaryEvent && new Date() <= endOf4thAnnivesaryEvent;
