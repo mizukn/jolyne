181a1681bbfc68ddb23530c2c99cce5873fe88b1
xMizuki <62658861+xMizuki@users.noreply.github.com>
2026-05-13 23:53:29 +0200

    Migrate vote command to V2 components

--- src/commands/social/Vote.ts
+++ src/commands/social/Vote.ts
@@ -1,162 +1,110 @@
 import { SlashCommandFile } from "../../@types";
-import { Message, InteractionResponse, APIEmbed, ButtonBuilder, ButtonStyle } from "discord.js";
+import { Message, InteractionResponse, ButtonBuilder, ButtonStyle } from "discord.js";
 import CommandInteractionContext from "../../structures/CommandInteractionContext";
+import * as Functions from "../../utils/Functions";
 import {
     generateDiscordTimestamp,
     TopGGVoteRewards,
     findItem,
     addXp,
     hasVotedRecenty,
-    actionRow,
 } from "../../utils/Functions";
 import { cloneDeep } from "lodash";
+import { containers, COLORS, SectionData } from "../../utils/containers";
 
 const voteForJolyneButton = new ButtonBuilder()
-    .setLabel("Vote for Jolyne")
+    .setLabel("Vote on Top.GG")
     .setStyle(ButtonStyle.Link)
+    .setEmoji("<:topgg:1502772726073397409>") // Use a placeholder or appropriate emoji if available
     .setURL("https://top.gg/bot/923619190831730698");
 
 const slashCommand: SlashCommandFile = {
     data: {
         name: "vote",
-        description: "Vote for Jolyne in Top.GG",
+        description: "Vote for Jolyne on Top.GG to earn rewards.",
         options: [],
     },
     execute: async (
         ctx: CommandInteractionContext
     ): Promise<Message | void | InteractionResponse> => {
+        const now = Date.now();
         const voteMonth = new Date().toLocaleString("en-US", { month: "long", year: "numeric" });
-        /*const previousMonthDateFormat = new Date(
-            `${new Date().getMonth() - 1}/1/${new Date().getFullYear()}`
-        ).toLocaleString("en-US", { month: "long", year: "numeric" });*/
-        // the code above is bad because if we're in january, it will return december of the previous year
-        // so we have to adapt
-        const month = new Date().getUTCMonth() - 1;
-        const year = new Date().getFullYear();
-        const previousMonthDateFormat = new Date(year, month, 1).toLocaleString("en-US", {
+        
+        // Handle previous month for streak/history display
+        const monthOffset = new Date().getUTCMonth() - 1;
+        const yearOffset = new Date().getFullYear();
+        const previousMonthDateFormat = new Date(yearOffset, monthOffset, 1).toLocaleString("en-US", {
             month: "long",
             year: "numeric",
         });
-        const previousMonthVotes =
-            ctx.userData.voteHistory[previousMonthDateFormat]?.sort((a, b) => a - b) ?? [];
+
+        const monthVotes = ctx.userData.voteHistory[voteMonth]?.sort((a, b) => a - b) ?? [];
+        const previousMonthVotes = ctx.userData.voteHistory[previousMonthDateFormat]?.sort((a, b) => a - b) ?? [];
         const totalVotes = ctx.userData.totalVotes || 0;
-        const monthVotes = ctx.userData.voteHistory[voteMonth]?.sort((a, b) => a - b) ?? []; // array of timestamps
+
+        const lastVote = monthVotes[monthVotes.length - 1] ?? previousMonthVotes[previousMonthVotes.length - 1] ?? 0;
+        const canVoteTimestamp = lastVote + 43200000; // 12 hours
+        const canVoteNow = now >= canVoteTimestamp;
 
         const voteRewards = TopGGVoteRewards(ctx.userData);
-        const lastVote =
-            cloneDeep(
-                monthVotes[monthVotes.length - 1] ??
-                    previousMonthVotes[previousMonthVotes.length - 1]
-            ) || 0;
-        const canVoteTimestamp = lastVote + 43200000; // 12 hours
-
         const xpRewards = addXp(ctx.userData, voteRewards.xp, ctx.client, true);
 
-        const embeds: APIEmbed[] = [
-            {
-                author: {
-                    icon_url:
-                        "https://pbs.twimg.com/profile_images/1502418247706046466/EMg2DjtV_400x400.jpg",
-                    name: "Top.GG",
-                },
-                description: `You have voted **${
-                    monthVotes.length
-                }** times this month and **${totalVotes}** times in total${
-                    monthVotes[monthVotes.length - 1] ??
-                    previousMonthVotes[previousMonthVotes.length - 1]
-                        ? " (last vote " +
-                          generateDiscordTimestamp(
-                              monthVotes[monthVotes.length - 1] ??
-                                  previousMonthVotes[previousMonthVotes.length - 1],
-                              "FROM_NOW"
-                          ) +
-                          ")"
-                        : ""
-                }.\nBy voting, you can earn **${voteRewards.coins.toLocaleString()}** ${
-                    ctx.client.localEmojis.jocoins
-                }, **${xpRewards.toLocaleString()}** ${ctx.client.localEmojis.xp}, x2 ${
-                    ctx.client.localEmojis.mysterious_arrow
-                } **Stand Arrows** and x2 ${
-                    ctx.client.localEmojis.mysterious_arrow
-                } **Rare Stand Arrows** + 1x ${
-                    findItem("dungeon").emoji
-                } **Dungeon Key** every 2 votes.${
-                    hasVotedRecenty(cloneDeep(ctx.userData), ctx.client)
-                        ? ""
-                        : `\nAlso, voting will reduce all your cooldowns to **45 seconds** for **5 minutes** (+1 minute if you're a [booster](https://discord.gg/jolyne-support-923608916540145694), +X minutes* where X equals your current tier if [you're an active patron](https://patreon.com/mizuki54)).`
-                }`,
-                color: 0xff3366,
-                fields: [],
-            },
-        ];
+        const sections: SectionData[] = [];
 
-        if (
-            canVoteTimestamp > Date.now() &&
-            (monthVotes.length > 0 || previousMonthVotes.length > 0)
-        ) {
-            const rewardXP = addXp(ctx.userData, voteRewards.xp, ctx.client, true);
-            embeds[0].fields.push({
-                // color: 0xff3366,
-                // fields: [
-                // {
-                name: "Thank you for voting!",
-                value: `You have been given **${voteRewards.coins.toLocaleString()}** ${
-                    ctx.client.localEmojis.jocoins
-                }, **${rewardXP.toLocaleString()}** ${ctx.client.localEmojis.xp}, x2 ${
-                    ctx.client.localEmojis.mysterious_arrow
-                } **Stand Arrows** for voting ${generateDiscordTimestamp(lastVote, "FROM_NOW")}. ${
-                    ctx.userData.totalVotes % 2 === 0
-                        ? `\nYou have also been giving x2 ${
-                              ctx.client.localEmojis.mysterious_arrow
-                          } Rare Stand Arrows and a ${
-                              findItem("dungeon").emoji
-                          } **Dungeon Key** for voting 2 times.`
-                        : `\nIf you vote 1 more time, you will be given x2 ${
-                              ctx.client.localEmojis.mysterious_arrow
-                          } Rare Stand Arrows and a ${findItem("dungeon").emoji} **Dungeon Key**.`
-                }`,
-                // }
-                // ]
-            });
-
-            if (hasVotedRecenty(cloneDeep(ctx.userData), ctx.client)) {
-                const expire = lastVote + 60000 * 5; // only lasts for 5 minutes
-                embeds[0].fields.push({
-                    name: `${ctx.client.localEmojis.timerIcon} Low Cooldown!`,
-                    value: `Since you have voted recently, all your cooldowns are reduced to **45 seconds** !! (${[
-                        "assault",
-                        "loot",
-                        "raid",
-                    ]
-                        .map((x) => ctx.client.getSlashCommandMention(x))
-                        .join(", ")}) [expires ${generateDiscordTimestamp(
-                        expire,
-                        "FROM_NOW"
-                    )}]\nYou have also been fully healed and your stamina has been restored.`,
-                });
-                return await ctx.makeMessage({ embeds });
-            }
+        // Section 1: Stats
+        let statsText = `### 📊 Voting Stats\n> **Month Votes:** ${monthVotes.length}\n> **Total Votes:** ${totalVotes}`;
+        if (lastVote > 0) {
+            statsText += `\n> **Last Vote:** ${generateDiscordTimestamp(lastVote, "FROM_NOW")}`;
         }
-        embeds[0].fields.push({
-            //color: 0xff3366,
-            //fields: [
-            //  {
-            name: "Vote for Jolyne",
-            value: `[${
-                hasVotedRecenty(cloneDeep(ctx.userData), ctx.client, 43200000)
-                    ? `You can vote again ${generateDiscordTimestamp(
-                          (monthVotes[monthVotes.length - 1] ??
-                              previousMonthVotes[previousMonthVotes.length - 1] ??
-                              Date.now()) + 43200000,
-                          "FROM_NOW"
-                      )}.`
-                    : `You can vote now!`
-            }](https://top.gg/bot/923619190831730698)`,
-            //}
-            //]
+        sections.push({ text: statsText });
+
+        // Section 2: Rewards
+        const dungeonEmoji = findItem("dungeon").emoji;
+        const arrowEmoji = ctx.client.localEmojis.mysterious_arrow;
+        
+        sections.push({
+            text: `### 🎁 Voting Rewards\n> **Coins:** ${voteRewards.coins.toLocaleString()} ${ctx.client.localEmojis.jocoins}\n> **XP:** ${xpRewards.toLocaleString()} ${ctx.client.localEmojis.xp}\n> **Items:** x2 ${arrowEmoji} Stand Arrows, x2 ${arrowEmoji} Rare Stand Arrows`
+        });
+        sections.push({
+            text: `### ⭐ Bonus Rewards\n> Every **2 votes**, you get a bonus **${dungeonEmoji} Dungeon Key**! ${ctx.userData.totalVotes % 2 === 0 ? "*(Next vote gives bonus)*" : "*(Earned on last vote)*"}`
+        })
+
+        // Section 3: Status / Perks
+        const cooldownPerkActive = hasVotedRecenty(cloneDeep(ctx.userData), ctx.client);
+        if (cooldownPerkActive) {
+            const expire = lastVote + 60000 * 5; // 5 minute base
+            sections.push({
+                text: `### ⚡ Perk: Low Cooldowns\n> All your cooldowns are reduced to **45 seconds**!\n> **Expires:** ${generateDiscordTimestamp(expire, "FROM_NOW")}\n> *Includes: Assault, Loot, and Raid.*`
+            });
+        } else {
+            sections.push({
+                text: `### 🕒 Voting Perks\n> Voting reduces all your cooldowns to **45 seconds** for **5 minutes**!\n> *Bonus time for boosters and patrons.*`
+            });
+        }
+
+        // Section 4: Call to Action
+        if (canVoteNow) {
+            sections.push({
+                text: `### 🗳️ Vote Now!\n> You can vote right now to claim your rewards and activate your perks.`
+            });
+        } else {
+            sections.push({
+                text: `### 🕒 Next Vote\n> You can vote again in ${generateDiscordTimestamp(canVoteTimestamp, "FROM_NOW")}.`
+            });
+        }
+
+        const reply = containers.primary({
+            title: "<:topgg:1502772726073397409> Vote for Jolyne",
+            description: "Support the bot and earn exclusive rewards and perks!",
+            descriptionDivider: true,
+            sections,
+            sectionDividers: true,
+            color: 0xff3366, // Top.GG Brand Color
         });
 
-        return await ctx.makeMessage({ embeds, components: [actionRow([voteForJolyneButton])] });
+        reply.components.push(Functions.actionRow([voteForJolyneButton]));
+
+        return await ctx.makeMessage(reply);
     },
 };
 
